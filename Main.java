a48c4ddc447851da6d2c2a453310b8d990ffbad6
NichFegley <26nfegley@students.bhasd.org>
2024-10-03 08:58:57 -0400

    finished project

--- Main.java
+++ Main.java
@@ -16,12 +16,31 @@ class Main {
   public static void main(String[] args) {
    // zero argument construct
    Pet pet1= new Pet();
+   System.out.println(pet1);
+   System.out.println();
 
    // three 6 arguement construct
    Pet pet2 = new Pet("cat","Bain",11,12.5,true,false);
-
-   System.out.println(pet1);
    System.out.println(pet2);
-
+   System.out.println();
+   Pet pet3= new Pet("dog","Whiskey",8,32,true,false);
+   System.out.println(pet3);
+   System.out.println();
+   Pet pet4 = new Pet("dog","Sunny",5,41,false,true);
+   System.out.println(pet4);
+  
+   // put pet2 to sleep
+   pet2.sleep();
+   System.out.println();
+   //wake pet4 up
+   pet4.wakeUp();
+   System.out.println();
+   //time passing
+   pet3.aging(1);
+   pet4.aging(1);
+   System.out.println();
+   //weight changes
+   pet4.changeWeight(2.5);
+   pet3.changeWeight(-0.4);
   }
 }
